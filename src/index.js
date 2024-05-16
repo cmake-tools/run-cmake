a4e8b7b54e5e1144139875f606920fc9dcef782f
flagarde <flagarde.programs@pm.me>
2024-05-16 21:55:01 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const exec = require('@actions/exec');
 
 try
 {
+  exec.exec('ln',['-s', '/usr/lib/x86_64-linux-gnu/libidn.so.12.x','/usr/lib/x86_64-linux-gnu/libidn.so.11')
   exec.exec('cmake','--version');
 }
 catch (error)
