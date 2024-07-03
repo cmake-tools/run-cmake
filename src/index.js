cbd68917cf2329fb59999ee5782263f74dfd8d24
flagarde <flagarde.programs@pm.me>
2024-07-03 14:39:32 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -252,7 +252,7 @@ class commandLineMaker
         ['3.29', ['Borland Makefiles','Green Hills MULTI','MinGW Makefiles','MSYS Makefiles','Ninja','Ninja Multi-Config','NMake Makefiles','NMake Makefiles JOM','Unix Makefiles','Visual Studio 9 2008','Visual Studio 12 2013','Visual Studio 14 2015','Visual Studio 15 2017','Visual Studio 16 2019','Visual Studio 17 2022','Watcom WMake']],
         ['3.30', ['Borland Makefiles','Green Hills MULTI','MinGW Makefiles','MSYS Makefiles','Ninja','Ninja Multi-Config','NMake Makefiles','NMake Makefiles JOM','Unix Makefiles','Visual Studio 12 2013','Visual Studio 14 2015','Visual Studio 15 2017','Visual Studio 16 2019','Visual Studio 17 2022','Watcom WMake']],
       ]);
-      version=semver.major(global.cmake_version)+'.'+semver.major(global.cmake_version)
+      let version=semver.major(global.cmake_version)+'.'+semver.major(global.cmake_version)
       return myMap.get(version.toString());
     }
     else if(process.platform === "linux")
@@ -290,7 +290,7 @@ class commandLineMaker
         ['3.29', ['Green Hills MULTI','Ninja','Ninja Multi-Config','Unix Makefiles','Watcom WMake']],
         ['3.30', ['Green Hills MULTI','Ninja','Ninja Multi-Config','Unix Makefiles','Watcom WMake']],
       ]);
-      version=semver.major(global.cmake_version)+'.'+semver.major(global.cmake_version)
+      let version=semver.major(global.cmake_version)+'.'+semver.major(global.cmake_version)
       return myMap.get(version.toString());
     }
     else
@@ -328,7 +328,7 @@ class commandLineMaker
         ['3.29', ['Ninja','Ninja Multi-Config','Unix Makefiles','Watcom WMake','Xcode']],
         ['3.30', ['Ninja','Ninja Multi-Config','Unix Makefiles','Watcom WMake','Xcode']],
       ]);
-      version=semver.major(global.cmake_version)+'.'+semver.major(global.cmake_version)
+      let version=semver.major(global.cmake_version)+'.'+semver.major(global.cmake_version)
       return myMap.get(version.toString());
     }
   }
