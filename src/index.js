687a0077757994283b80c3a220148235d307fe52
flagarde <flagarde.programs@pm.me>
2024-07-12 13:35:53 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -459,7 +459,7 @@ class CommandLineMaker
     {
       core.exportVariable('config',config)
       if(CMakeVersionGreaterEqual('3.15.0')) return Array('--config',config)
-      else return Array('-DBUILD_TYPE:STRING'+config)
+      else return Array('-DBUILD_TYPE:STRING='+config)
     }
     else return []
   }
