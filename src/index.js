fba05a4794f073a71aa531c976a49ef7312c4469
flagarde <flagarde.programs@pm.me>
2024-08-04 01:29:40 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -24,8 +24,8 @@ async function fixes()
       }
     }
     options.silent = false
-    await exec.exec('sudo apt-get install --no-install-recommends -y libidn12', [], options)
-    await exec.exec('ln -sf /usr/lib/x86_64-linux-gnu/libidn.so.12 /usr/lib/x86_64-linux-gnu/libidn.so.11', [], options)
+    await exec.exec('sudo apt-get update -y; sudo apt-get install --no-install-recommends -y libidn12', [], options)
+    await exec.exec('sudo ln -sf /usr/lib/x86_64-linux-gnu/libidn.so.12 /usr/lib/x86_64-linux-gnu/libidn.so.11', [], options)
   }
 
 }
