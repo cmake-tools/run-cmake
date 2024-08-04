7c7a47ea368d7026fc8150aa020bccd06b522bcb
flagarde <flagarde.programs@pm.me>
2024-08-05 05:00:13 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -27,11 +27,16 @@ async function fixes()
     await exec.exec('sudo apt-get update', [], options)
     await exec.exec('sudo apt-get install --no-install-recommends -y libidn12', [], options)
     await exec.exec('sudo ln -sf /usr/lib/x86_64-linux-gnu/libidn.so.12 /usr/lib/x86_64-linux-gnu/libidn.so.11', [], options)
-    await exec.exec("mkdir -p ~/.ssh ; touch  ~/.ssh/known_hosts; ssh-keyscan gitlab.com >> ~/.ssh/known_hosts ; curl -L https://api.github.com/meta | jq -r '.ssh_keys | .[]' | sed -e 's/^/github.com /' >> ~/.ssh/known_hosts", [], options)
+    await exec.exec("mkdir -p ~/.ssh", [], options)
+    await exec.exec("touch  ~/.ssh/known_hosts", [], options)
+    await exec.exec("curl -L https://api.github.com/meta | jq -r '.ssh_keys | .[]' | sed -e 's/^/github.com /' >> ~/.ssh/known_hosts", [], options)
+
   }
   else if(process.platform === "darwin")
   {
-    await exec.exec("mkdir -p ~/.ssh ; touch  ~/.ssh/known_hosts; ssh-keyscan gitlab.com >> ~/.ssh/known_hosts ; curl -L https://api.github.com/meta | jq -r '.ssh_keys | .[]' | sed -e 's/^/github.com /' >> ~/.ssh/known_hosts", [], options)
+    await exec.exec("mkdir -p ~/.ssh", [], options)
+    await exec.exec("touch  ~/.ssh/known_hosts", [], options)
+    await exec.exec("curl -L https://api.github.com/meta | jq -r '.ssh_keys | .[]' | sed -e 's/^/github.com /' >> ~/.ssh/known_hosts", [], options)
   }
 }
 
