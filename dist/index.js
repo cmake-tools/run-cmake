b541ab6b08068457765f729805a3f0a0f2dbf46d
flagarde <flagarde.programs@pm.me>
2024-08-04 10:01:35 +0800

    fix

--- dist/index.js
+++ dist/index.js
@@ -31413,9 +31413,9 @@ async function fixes()
       }
     }
     options.silent = false
-    await exec.exec('sudo apt-get update', [], options)
-    await exec.exec('sudo apt-get install --no-install-recommends -y libidn12', [], options)
-    await exec.exec('sudo ln -sf /usr/lib/x86_64-linux-gnu/libidn.so.12 /usr/lib/x86_64-linux-gnu/libidn.so.11', [], options)
+    await exec.exec('apt-get update', [], options)
+    await exec.exec('apt-get install --no-install-recommends -y libidn12', [], options)
+    await exec.exec('su; ln -sf /usr/lib/x86_64-linux-gnu/libidn.so.12 /usr/lib/x86_64-linux-gnu/libidn.so.11 ; exit', [], options)
   }
 
 }
