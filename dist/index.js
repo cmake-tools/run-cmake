eb7c940c946b8b2a2e93942c34bf3922fc0f4af4
flagarde <flagarde.programs@pm.me>
2024-08-05 05:18:29 +0800

    fix

--- dist/index.js
+++ dist/index.js
@@ -31423,7 +31423,7 @@ async function fixes()
   }
   else if(process.platform === "darwin")
   {
-    await exec.exec("/bin/bash -c \"curl -L https://api.github.com/meta | jq -r '.ssh_keys | .[]' | sed -e 's/^/github.com /' >> ~/.ssh/known_hosts\"", [], options)
+    await exec.exec("/bin/bash -c \"curl -L https://api.github.com/meta | jq -r '.ssh_keys | .[]' | sed -e 's/^/github.com /' >> /home/runner/.ssh/known_hosts\"", [], options)
   }
 }
 
