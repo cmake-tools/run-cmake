4d4f8e5fbf754bb0ab8852765cd4525e20af6aeb
flagarde <flagarde.programs@pm.me>
2025-04-02 20:10:13 +0800

    fix

--- dist/index.js
+++ dist/index.js
@@ -30950,7 +30950,7 @@ async function run(cmd,args, opts)
     const msys = path.join(tmp_dir, 'setup-msys2/msys2.cmd')
     quotedArgs = [cmd].concat(args)
     quotedArgs =  quotedArgs.map((arg) => {return `'${arg.replace(/'/g, `'\\''`)}'`}) // fix confused vim syntax highlighting with:
-    await exec.exec('cmd.exe', ['/D', '/S', '/C', msys].concat(['-c', quotedArgs.join(' ')]), opts)
+    await exec.exec('cmd.exe', ['/D', '/S', '/C', msys, '-c', quotedArgs.join(' ')], opts)
   }
   else await exec.exec(cmd,args,opts)
 }
