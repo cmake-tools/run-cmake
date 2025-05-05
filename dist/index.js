078c2fd609c26c0f4ab5404d5bd3a26001d3f995
flagarde <flagarde.programs@pm.me>
2025-05-05 20:29:28 +0800

    fix

--- dist/index.js
+++ dist/index.js
@@ -34019,19 +34019,22 @@ async function os_is()
 
 async function fixCMake()
 {
-  let ret;
-  let options = {};
-  options.silent = true
-  if( await os_is() === "linux")
+  if(!global.fix_done)
   {
-    ret = await exec.exec('cmake --help', [], options)
-    if(ret!=0)
+    let ret;
+    let options = {};
+    options.silent = true
+    if( await os_is() === "linux")
     {
-      ret = await exec.exec('sudo apt-get update', [], options)
-      ret = await exec.exec('sudo apt-get install --no-install-recommends -y libidn12', [], options)
-      ret = await exec.exec('sudo ln -sf /usr/lib/x86_64-linux-gnu/libidn.so.12 /usr/lib/x86_64-linux-gnu/libidn.so.11', [], options)
+      ret = await exec.exec('cmake --help', [], options)
+      if(ret!=0)
+      {
+        ret = await exec.exec('sudo apt-get update', [], options)
+        ret = await exec.exec('sudo apt-get install --no-install-recommends -y libidn12', [], options)
+        ret = await exec.exec('sudo ln -sf /usr/lib/x86_64-linux-gnu/libidn.so.12 /usr/lib/x86_64-linux-gnu/libidn.so.11', [], options)
+      }
+      global.fix_done = true;
     }
-    global.fix_done = true;
   }
   return true;
 }
