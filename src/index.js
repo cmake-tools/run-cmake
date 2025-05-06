c6bb06c94387cce21520fa0fc607eeb1d324d430
flagarde <flagarde.programs@pm.me>
2025-05-07 05:35:03 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -1035,12 +1035,9 @@ async function runCMake(args,options)
 {
   if(is_msys2())
   {
-    //let out = JSON.parse(process.env.GITHUB_OUTPUT)
-    let toto =core.getInput('msys2-location', { required: true });
-    console.log(`toto ${toto}`)
-    //const tmp_dir = process.env['RUNNER_TEMP'];
-    const msys = path.join(core.getInput('msys2-location'), 'setup-msys2/msys2.cmd')
-    return exec.exec('cmd', ['/D', '/S', '/C', msys, '-c','cmake', args.join(' ')], options)
+    const tmp_dir = process.env['RUNNER_TEMP'];
+    const msys = path.join(tmp_dir, 'setup-msys2/msys2.cmd')
+    return exec.exec('cmd', ['/D', '/S', '/C', msys, '-c','cmake'].concat(args), options)
   }
   else return exec.exec('cmake',args,options)
 }
