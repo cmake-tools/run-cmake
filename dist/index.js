82e76176480dc67377bda26f1e01a89dd3b5bd0a
flagarde <flagarde.programs@pm.me>
2025-05-06 16:05:44 +0800

    fix

--- dist/index.js
+++ dist/index.js
@@ -34351,7 +34351,7 @@ class CommandLineMaker
     this.install_prefix = core.getInput('install_prefix', { required: false, default:'' });
     if(this.install_prefix!='')
     {
-      if(process.env.MSYS2_LOCATION)this.install_prefix=path.resolve(process.env.MSYS2_LOCATION,this.install_prefix)
+      if(process.env.MSYS2_LOCATION)this.install_prefix=path.resolve('/usr/local/',this.install_prefix)
       else this.install_prefix=path.resolve(this.install_prefix)
       if(CMakeVersionGreaterEqual('3.21.0')) return Array('--install-prefix',this.install_prefix)
       else return Array('-DCMAKE_INSTALL_PREFIX:PATH='+this.install_prefix)
